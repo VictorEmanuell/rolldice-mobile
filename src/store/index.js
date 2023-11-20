0546e43fa8918b128c9ebd69fb49faa493f37556
VictorEmanuell <victoremanuelexx@gmail.com>
2023-11-20 20:45:37 -0300

    increment_reducers_in_index_store

--- src/store/index.js
+++ src/store/index.js
@@ -3,11 +3,17 @@ import { configureStore } from "@reduxjs/toolkit";
 import { loadingSlice } from "./Loading/slice";
 import { userSlice } from "./user/slice";
 import { characterSlice } from "./character/slice";
+import { armorSlice } from "./Armor/slice";
+import { attacksSlice } from "./Attacks/slice";
+import { skillsSlice } from "./Skills/slice";
 
 export const store = configureStore({
   reducer: {
     loading: loadingSlice.reducer,
     user: userSlice.reducer,
     character: characterSlice.reducer,
+    armor: armorSlice.reducer,
+    attacks: attacksSlice.reducer,
+    skills: skillsSlice.reducer,
   },
 });
